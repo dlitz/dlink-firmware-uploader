20983966c3f0751813f986797b8508d613f7ed7d
Tom Hebb <tch@fb.com>
2021-12-30 10:18:21 -0800

    Target 512-byte segments, adjust payload to match
    New versions of Linux seem to prefer 512-byte TCP segments to 1024-byte
    ones, meaning this script doesn't work properly anymore.
    
    In theory, fixing this would just be a matter of changing BUF_SIZE to
    512 and going on our way. However, when I did that, the router (a
    DIR-615 E3) rejected my uploads as invalid. After digging into the
    source code (see the comments in this patch for file paths), I
    discovered that the firmware has special paths that only run for
    segments shorter than 1024 bytes (`uip_datalen() != 1446`), and the
    different behavior stopped our payload from being interpreted correctly.
    
    After far too much time reading the source to make sense of its
    intentions, I came to the new payload format in this patch, which should
    work well for any segment size under 1024. The new payload doesn't
    conform to the HTTP spec, since I included only exactly what's needed to
    make the server happy; this makes it easier to compare against the code
    and see what's going wrong in case things break again, IMO.
    
    Tested on a DIR 615 E3 in emergency download mode on Linux 5.15.10.

--- upload.py
+++ upload.py
@@ -109,9 +109,18 @@
 # time we fail to get an ACK.
 #
 # Additionally, there are the following bugs:
-# - The header needs to be in a different TCP segment from the body, or the
-#   firmware won't load properly.
 # - We need to get our final data ACK before sending TCP FIN.
+# - The server on the device requires certain pieces of data to either share
+#   or not share a segment, contrary to the stream-oriented nature of TCP.
+#   These requirements are not consistent; they change based on heuristics
+#   like the User-Agent header and the specific boundary string chosen for
+#   the MIME multipart POST data. The segmentation and header values chosen by
+#   this script are one way to get on a "happy path" through the server, but
+#   they're not the only way. For example, we could send a Firefox user agent
+#   and structure our segments how the server expects Firefox to send them,
+#   and that would work too. If you want to dive into the madness, see
+#   Matrix/platform/kernel/AR7240/loader/u-boot/httpd/cgi.c in D-Link's
+#   DIR-615 E4 GPL release.
 #
 # So, we need this stupid program to work around these bugs.
 
@@ -129,8 +138,10 @@ from termios import TIOCOUTQ as SIOCOUTQ    # On Linux, SIOCOUTQ has the same va
 
 # We're going to try hard not to have more than one segment in flight at a time,
 # since that's where the ack bug shows up. Unfortunately, we have very little
-# control over how Linux decides to segment things.
-BUF_SIZE = 1024
+# control over how Linux decides to segment things. Testing has shown that on
+# kernel 5.15.10, at least, segments will reliably be at least 512 bytes, so
+# let's pick that.
+BUF_SIZE = 512
 
 
 filename = sys.argv[1]
@@ -144,29 +155,22 @@ data['filename'] = os.path.basename(filename).encode('utf-8')
 data['content'] = content
 
 header_template = b"""\
-POST /cgi/index HTTP/1.1
-Host: 192.168.0.1
-Accept: */*
-Content-Type: multipart/form-data; boundary={boundary}
+POST /cgi/index
 Content-Length: {body_length}
-Connection: close
-Cache-Control: no-cache
 
 """.replace(b'\n', b'\r\n')
 
 body_template = b"""\
---{boundary}
-Content-Disposition: form-data; name="files"; filename="{filename}"
-Content-Type: application/octet-stream
+----------
+Content-Type:
 
 {content}
---{boundary}--
+------------
 """.replace(b'\n', b'\r\n')
 
-body = data['body'] = body_template.format(**data)
-data['body_length'] = len(data['body'])
-header = header_template.format(**data)
-request = header + body
+body = body_template.format(**data)
+data['body_length'] = len(body)
+request = header_template.format(**data) + body
 
 # Connect, then wait a bit.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -206,10 +210,10 @@ while True:
 
     assert i <= len(request)
 
-    # When we finish sending the header or the body, or when we receive EOF,
-    # flush the output buffer (select() already returned, so we have to
-    # busy-wait at this point).
-    if i == len(header) or i == len(request) or eof:
+    # When we finish sending the body, or when we receive EOF, flush the output
+    # buffer (select() already returned, so we have to busy-wait at this
+    # point).
+    if i == len(request) or eof:
         # Busy-wait until all data have been sent
         ioctl(s, SIOCOUTQ, ioctl_buf, 1)
         bytes_remaining = ioctl_buf[0]
@@ -221,9 +225,7 @@ while True:
             bytes_remaining = ioctl_buf[0]
         print("Output buffer drained.")
 
-        if i == len(header):
-            print("\nDone sending request header (%d bytes)" % (i,))
-        elif i == len(request):
+        if i == len(request):
             print("\nDone uploading all %d bytes." % (i,))
             break
         else:
